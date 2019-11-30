20b081923ee1876140bd92c42ca3ff3e35caaeb9
DolphieDude <andreycherevat@gmail.com>
2019-11-30 12:50:22 +0200

    Stack task

--- src/com/cherevatyi/Main.java
+++ src/com/cherevatyi/Main.java
@@ -1,8 +1,17 @@
 package com.cherevatyi;
 
+import java.util.Stack;
+
 public class Main {
-
     public static void main(String[] args) {
-
+        Stack<Integer> stack = new Stack<Integer>();
+        System.out.println(stack.empty());
+        stack.push(55);
+        stack.push(0);
+        System.out.println(stack.empty());
+        System.out.println(stack.peek());
+        for (Integer n: stack) System.out.println(n);
+        Integer num = stack.pop();
+        System.out.println(num);
     }
 }
