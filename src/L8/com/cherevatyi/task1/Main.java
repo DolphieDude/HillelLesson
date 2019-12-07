693ec5950e56b3ed87f8da27ba3f7ef8c3dd91c4
DolphieDude <andreycherevat@gmail.com>
2019-12-07 13:01:53 +0200

    task2 with exception handling

--- src/L8/com/cherevatyi/task1/Main.java
+++ src/L8/com/cherevatyi/task1/Main.java
@@ -1,7 +1,25 @@
 package L8.com.cherevatyi.task1;
 
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-
+        System.out.println(Arrays.toString(Thread.currentThread().getStackTrace()));
+        first();
+    }
+    public static void first () {
+        System.out.println(Arrays.toString(Thread.currentThread().getStackTrace()));
+        second();
+    }
+    public static void second () {
+        System.out.println(Arrays.toString(Thread.currentThread().getStackTrace()));
+        third();
+    }
+    public static void third () {
+        System.out.println(Arrays.toString(Thread.currentThread().getStackTrace()));
+        fourth();
+    }
+    public static void fourth () {
+        System.out.println(Arrays.toString(Thread.currentThread().getStackTrace()));
     }
 }
---
+++ src/L8/com/cherevatyi/task2/Main.java
@@ -0,0 +1,19 @@
+package L8.com.cherevatyi.task2;
+
+public class Main {
+    public static void main(String[] args) {
+        try {
+            System.out.println("Throw");
+            exceptionThrower();
+            System.out.println("FeelsBad");
+        }
+        catch (Exception e) {
+            System.out.println("HA! Caught");
+        }
+    }
+    public static void exceptionThrower () {
+        System.out.println("Before 0");
+        int nothing = 1 / 0;
+        System.out.println("After 0");
+    }
+}
