55cf3252f8201ff1ce1ee4677cf3f2cc64edd1b1
DolphieDude <andreycherevat@gmail.com>
2019-12-14 12:52:06 +0200

    task 1 testing static block

--- src/L9/task1/Main.java
+++ src/L9/task1/Main.java
@@ -1,7 +1,19 @@
 package L9.task1;
 
-public class Main {
-    public static void main(String[] args) {
+import java.util.*;
 
+public class Main {
+    private static Map<String, Integer> staticMap = new HashMap<>();
+    static {
+        staticMap.put("monday", 1);
+        staticMap.put("tuesday", 2);
+        staticMap.put("wednesday", 3);
+        staticMap.put("thursday", 4);
+        staticMap.put("friday", 5);
+        staticMap.put("saturday", 6);
+        staticMap.put("sunday", 7);
+    }
+    public static void main(String[] args) {
+        System.out.println(staticMap.get("tuesday"));
     }
 }
