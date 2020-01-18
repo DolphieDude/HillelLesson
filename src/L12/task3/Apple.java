0f08b701606d9c1b573bca4b2ce7174ff996bd34
DolphieDude <andreycherevat@gmail.com>
2020-01-18 16:11:42 +0200

    Enums

--- src/L12/task3/Apple.java
+++ src/L12/task3/Apple.java
@@ -1,5 +1,14 @@
 package L12.task3;
 
 public enum Apple {
-    Jonathan, GoldenDel, RedDel, Winesap, Cortland
+    Jonathan(5), GoldenDel(10), RedDel(7), Winesap(19), Cortland(11);
+    private int price;
+
+    public int getPrice() {
+        return price;
+    }
+
+    Apple(int price) {
+        this.price = price;
+    }
 }
